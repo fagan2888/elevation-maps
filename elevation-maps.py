c3ed4c0c848d83f91823e01d44c88ce36c184b48
mhlinder <mhlinder@gmail.com>
2013-11-05 15:16:53 -0500

    calculates total increase, total decrease, distance, and numsteps for each suggested route

--- elevation-maps.py
+++ elevation-maps.py
@@ -1,85 +1,125 @@
 # m henry linder, 11/3/13
 # run api request to google maps
 import json, urllib2
-from pandas import DataFrame
+from pandas import DataFrame, Series
 import numpy as np
 import pickle
 
-# def elevation_request(locations, output, sensor):
-    # # submit elevation api request
-    # query = "http://maps.googleapis.com/maps/api/elevation/%s?" % (output)
-    # query = query + "&sensor=%s" % sensor
-    # query = query + "&locations="
-    # for location in locations.values:
-        # lat = location[0]
-        # lon = location[1]
-        # if query[-1] != "=":
-            # query = query + "|"
-        # query = query + "%f,%f" % (lat, lon)
+def elevation_request(locations, output, sensor):
+    # submit elevation api request
+    query = "http://maps.googleapis.com/maps/api/elevation/%s?" % (output)
+    query = query + "&sensor=%s" % sensor
+    query = query + "&locations="
+    for location in locations.values:
+        lat = location[0]
+        lon = location[1]
+        if query[-1] != "=":
+            query = query + "|"
+        query = query + "%f,%f" % (lat, lon)
 
-    # request = urllib2.urlopen(query)
-    # html = request.read()
-    # data = json.loads(html)
-    # return data
-
-
-# # main loop: loop through different transportation types
-# proposals = {}
-# points = []
-# transit = ['bicycling','walking','driving']
-# for method in transit:
-    # proposals[method] = {}
-    # # # options
-    # output = "json"
-    # sensor = "false"
-
-    # # start = raw_input('Start location: ')
-    # # end = raw_input('End location: ')
-    # start = "200 E 66th St., NY NY"
-    # end = "36 W 15th St. NY NY"
-    # start = start.replace(" ", "+")
-    # end = end.replace(" ", "+")
+    request = urllib2.urlopen(query)
+    html = request.read()
+    data = json.loads(html)
+    return data
 
 
-    # # defined at https://developers.google.com/maps/documentation/directions/
-    # parameters = "origin=%s" % start
-    # parameters = parameters + "&destination=%s" % end
-    # parameters = parameters + "&sensor=%s" % sensor
-    # parameters = parameters + "&avoid=highways"
-    # parameters = parameters + "&travelMode=%s" % method # setting to walking reduces number of alternatives
-    # parameters = parameters + "&alternatives=true"
-    # query = ("http://maps.googleapis.com/maps/api/directions/%s?%s") % (output, parameters)
+# main loop: loop through different transportation types
+proposals = {}
+statistics = {}
+points = []
 
-    # # # Obtain data
-    # request = urllib2.urlopen(query)
-    # html = request.read()
+transit = ['bicycling','walking','driving']
+for method in transit:
+    proposals[method] = {}
+    statistics[method] = {}
+    # # options
+    output = "json"
+    sensor = "false"
 
-    # data = json.loads(html)
+    # start = raw_input('Start location: ')
+    # end = raw_input('End location: ')
+    start = "200 E 66th St., NY NY"
+    end = "36 W 15th St. NY NY"
+    start = start.replace(" ", "+")
+    end = end.replace(" ", "+")
 
-    # # # Route process
-    # routes = data['routes']
-    # for route in routes: # each possible route
-        # rsumm = route['summary']
-        # proposals[method][rsumm] = [] # steps for this route
-        # legs = route['legs'] # generally, there will only be one leg
-        # for leg in legs:
-            # steps = leg['steps']
-            # for step in steps:
-                # # record start, end
-                # step_loc = []
-                # step_loc.append(step['start_location'])
-                # step_loc.append(step['end_location'])
-                # proposals[method][rsumm].append(step_loc)
 
-                # # keep each point
-                # points.append(step['start_location'])
-            # points.append(step['end_location']) # add last location, tok
-# points = DataFrame(points)
-# points.columns = ['lat','lon']
+    # defined at https://developers.google.com/maps/documentation/directions/
+    parameters = "origin=%s" % start
+    parameters = parameters + "&destination=%s" % end
+    parameters = parameters + "&sensor=%s" % sensor
+    parameters = parameters + "&avoid=highways"
+    parameters = parameters + "&travelMode=%s" % method # setting to walking reduces number of alternatives
+    parameters = parameters + "&alternatives=true"
+    query = ("http://maps.googleapis.com/maps/api/directions/%s?%s") % (output, parameters)
 
-# elevations = elevation_request(points, output, sensor)
-# pickle.dump(elevations, open('elevations.p','wb'))
-# pickle.dump(points, open('points.p','wb'))
+    # # Obtain data
+    request = urllib2.urlopen(query)
+    html = request.read()
+
+    data = json.loads(html)
+
+    # # Route process
+    routes = data['routes']
+    for route in routes: # each possible route
+        rsumm = route['summary']
+        proposals[method][rsumm] = [] # steps for this route
+        statistics[method][rsumm] = {}
+        legs = route['legs'] # generally, there will only be one leg
+        for leg in legs:
+            steps = leg['steps']
+            distance = 0
+            for step in steps:
+                # record start, end
+                step_loc = []
+                step_loc.append(step['start_location'])
+                step_loc.append(step['end_location'])
+                proposals[method][rsumm].append(step_loc)
+                distance = distance + step['distance']['value']
+
+                # keep each point
+                points.append(step['start_location'])
+            statistics[method][rsumm]['distance'] = distance
+            points.append(step['end_location']) # add last location, too
+points = DataFrame(points)
+points.columns = ['lat','lon']
+step_copy = step
+
+elevations = elevation_request(points, output, sensor)
+pickle.dump(elevations, open('elevations.p','wb'))
+pickle.dump(points, open('points.p','wb'))
+pickle.dump(proposals, open('proposals.p','wb'))
 
 elevations = pickle.load(open('elevations.p','rb'))
-points = pickle.load(open('points.p','rb')
+points = pickle.load(open('points.p','rb'))
+proposals = pickle.load(open('proposals.p','rb'))
+
+elevations = elevations['results']
+
+points['elevation'] = Series(index=points.index)
+
+for i in np.arange(len(elevations)):
+    points.ix[i]['elevation'] = elevations[i]['elevation']
+
+i = 0
+for mode in proposals.keys():
+    for route in proposals[mode].keys():
+        numsteps = len(route)
+        increase_total = 0
+        decrease_total = 0
+        for step in proposals[mode][route]:
+            start_pt = points[np.all([points['lat']==step[0]['lat'],
+                                      points['lon']==step[0]['lng']],0)].iloc[0]
+            start_elev = start_pt['elevation']
+            end_pt = points[np.all([points['lat']==step[1]['lat'],
+                                    points['lon']==step[1]['lng']],0)].iloc[0]
+            end_elev = end_pt['elevation']
+
+            diff = end_elev - start_elev
+            if diff >= 0: # increase in height
+                increase_total = increase_total + diff
+            else: # decrease in height
+                decrease_total = decrease_total + diff
+        statistics[mode][route]['numsteps'] = numsteps
+        statistics[mode][route]['increase_total'] = increase_total
+        statistics[mode][route]['decrease_total'] = decrease_total
