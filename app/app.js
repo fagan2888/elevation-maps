aeb29163fb6564af9a50419624b3bd1787718680
M. Henry Linder <mhlinder@gmail.com>
2014-03-02 21:22:37 -0500

    gets points, distances for each alternative route

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ var express = require('express'),
 app.listen(8000);
 
 app.get('/getopts', function (req, res) {
+    // parameters for directions
     var origin = '200+E+66th+St.,+NY+NY',
         destination = '600+Lincoln+Pl.,+Brooklyn+NY',
         mode = 'driving';
@@ -25,8 +26,6 @@ app.get('/getopts', function (req, res) {
         method: 'GET'
     };
 
-    var routes = [];
-    var i;
     var req = http.request(opts, function(res) {
         res.setEncoding('utf8');
 
@@ -36,31 +35,41 @@ app.get('/getopts', function (req, res) {
         });
 
         res.on('end', function() {
+            var points = [],
+                distances = [];
+
             data = JSON.parse(data);
-            dists(data);
+            dists(data, points, distances);
+
+            console.log(String(points.length));
+            console.log(String(distances));
         });
     });
 
     req.end();
-
-    function dists(data) {
-        var points = [];
-        rs = data.routes;
-
-        for (i = 0; i < rs.length; i++) {
-            var route = rs[i];
-
-            for (var j = 0; j < route.legs.length; j++) {
-
-                var leg = route.legs[j];
-                var step;
-                for (var k = 0; k < leg.steps.length; k++) {
-
-                    step = leg.steps[k];
-                    points.push(step.start_location);
-                };
-                points.push(step.end_location);
-            };
-        };
-    };
+    res.send();
 });
+
+function dists(data, points, distances) {
+    rs = data.routes;
+
+    for (var i = 0; i < rs.length; i++) {
+        var route = rs[i],
+            rtePoints = [],
+            distance = 0;
+        for (var j = 0; j < route.legs.length; j++) {
+
+            var leg = route.legs[j];
+            distance += leg.distance.value;
+
+            var step;
+            for (var k = 0; k < leg.steps.length; k++) {
+                step = leg.steps[k];
+                rtePoints.push(step.start_location);
+            };
+            rtePoints.push(step.end_location);
+        };
+        points.push(rtePoints);
+        distances.push(distance);
+    };
+};
